85e766b17775b63000082651c7067c9bb10dba87
Muhammad Alvin <45812297+Saiyanmonkey@users.noreply.github.com>
2021-01-13 19:40:21 +0700

    com

---
+++ MUHAMMAD_ALVIN_PRAMADRIAN/MazeD/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Maze mazePrefabs;
+
+    private Maze mazeInstance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BeginGame();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RestartGame();
+        }
+    }
+
+    private void BeginGame() {
+        mazeInstance = Instantiate(mazePrefabs) as Maze;
+        StartCoroutine(mazeInstance.Generate());
+    }
+
+    private void RestartGame() {
+        StopAllCoroutines();
+        Destroy(mazeInstance.gameObject);
+        BeginGame();
+    }
+}
